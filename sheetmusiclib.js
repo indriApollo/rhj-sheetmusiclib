dfcef202d7fe448776261854a179a09e3f593055
Raphael Leroux <indri.apollo@gmail.com>
2018-01-07 20:52:59 +0100

    remove unneeded dependencies

--- sheetmusiclib.js
+++ sheetmusiclib.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const http = require('http'); // no https for server because we are behind a proxy
-const urlHelper = require('url');
-const querystring = require('querystring');
+const http = require("http"); // no https for server because we are behind a proxy
+const urlHelper = require("url");
+const querystring = require("querystring");
 const conf = require("./configloader.js");
-const common = require("./common.js");
+const cm = require("./common.js");
 
 const httpGetHandler = require("./httpGetHandler.js")
 const httpPostHandler = require("./httpPostHandler.js")
@@ -25,48 +25,48 @@ http.createServer(function(request, response) {
 
     var body = [];
     
-    response.on('error', function(err) {
+    response.on("error", function(err) {
         console.error(err);
     });
     
-    request.on('error', function(err) {
+    request.on("error", function(err) {
         console.error(err);
         response.statusCode = 500;
         response.end();
     
-    }).on('data', function(chunk) {
+    }).on("data", function(chunk) {
         body.push(chunk);
-    }).on('end', function() {
+    }).on("end", function() {
         body = Buffer.concat(body).toString();
     
         switch(method) {
-            case 'GET':
+            case "GET":
                 httpGetHandler(conf, pathname, query, headers, response);
                 break;
 
-            case 'POST':
+            case "POST":
                 httpPostHandler(conf, pathname, query, headers, response);
                 break;
 
-            case 'PUT':
+            case "PUT":
                 httpPutHandler(conf, pathname, query, headers, response);
                 break;
                 
-            case 'DELETE':
+            case "DELETE":
                 httpDeleteHandler(conf, pathname, query, headers, response);
                 break;
     
-            case 'OPTIONS':
+            case "OPTIONS":
                 handleCORS(response);
                 break;
     
             default:
-                common.respond(response, "Unsupported http method", 400);
+                cm.respond(response, "Unsupported http method", 400);
                 break;
         }
     });
 }).listen(conf.get("SERVER_PORT"));
-console.log("server listening on port "+conf.get("SERVER_PORT"));
+console.log("server listening on port", conf.get("SERVER_PORT"));
 
 function handleCORS(response) {
     
@@ -79,10 +79,10 @@ function handleCORS(response) {
     // The preflighted requests expects http 200 for a successful request
     response.statusCode = 200;
     // We allow requests from any origin
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    // We have to explicitly allow Auth-Token since it's a custom header
-    response.setHeader('Access-Control-Allow-Headers', 'Auth-Token,User-Agent,Content-Type'); //can't use * !
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    // We have to explicitly allow Auth-Token since it"s a custom header
+    response.setHeader("Access-Control-Allow-Headers", "Auth-Token,User-Agent,Content-Type"); //can"t use * !
     // We allow POST, GET, PUT, DELETE and OPTIONS http methods
-    response.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     response.end();
 }
